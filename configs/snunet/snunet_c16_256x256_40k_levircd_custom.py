63405a5e5535e4154c7e3cfcfababfd4c6b239de
qbzju <2112303119@mail2.gdut.edu.cn>
2024-11-15 13:33:12 +0800

    update optimizer

--- configs/snunet/snunet_c16_256x256_40k_levircd_custom.py
+++ configs/snunet/snunet_c16_256x256_40k_levircd_custom.py
@@ -21,3 +21,9 @@ test_dataloader = dict(
         data_root=data_root
     )
 )
+
+# AdamW optimizer, no weight decay for position embedding & layer norm in backbone
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
